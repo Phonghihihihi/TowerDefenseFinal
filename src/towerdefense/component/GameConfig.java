fd80e832828c042cecab2d982ec4da133440db9d
khang <43407475+khangneko@users.noreply.github.com>
2019-12-03 15:42:20 +0700

    spawn enemies

--- src/towerdefense/component/GameConfig.java
+++ src/towerdefense/component/GameConfig.java
@@ -1,6 +1,6 @@
 package towerdefense.component;
 
-public class CommonFunc {
+public class GameConfig {
     public static final String GAME_NAME = "Tower Defense";
     public static final long TILE_SIZE = 64;
     private static final long TILE_HORIZONTAL = 20;
@@ -15,6 +15,8 @@ public class CommonFunc {
     public static final int MAP_TILE = (int) (TILE_VERTICAL*TILE_HORIZONTAL);
     public static final long CANVAS_WIDTH = TILE_SIZE * TILE_HORIZONTAL + UI_HORIZONTAL;
     public static final long CANVAS_HEIGHT = TILE_SIZE * TILE_VERTICAL;
+
+    public static final long START_MONEY = 50;
 
     private final double SHOT_FADE_TIME =1.;
 
