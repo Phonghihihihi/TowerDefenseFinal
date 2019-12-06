81571a3183f0ffa31e97b222a7fc2392f4a32736
=hoai thu <=hoaithu3t@gmail.com>
2019-12-06 17:38:47 +0700

    add enemy

--- src/towerdefense/component/enemy/SmallerEnemy.java
+++ src/towerdefense/component/enemy/SmallerEnemy.java
@@ -6,15 +6,16 @@ import towerdefense.component.GameConfig;
 import towerdefense.ui.TowerDefense;
 
 
-public class NormalEnemy extends AbstractEnemy implements Enemy
+
+public class SmallerEnemy extends AbstractEnemy implements Enemy
 {
-    public NormalEnemy(double posX, double posY, double width, double height) {
+    public SmallerEnemy(double posX, double posY, double width, double height) {
         super( posX, posY, width, height);
-        this.health = GameConfig.NORMAL_ENEMY_HEALTH;
-        this.armor = GameConfig.NORMAL_ENEMY_ARMOR;
-        this.speed = GameConfig.NORMAL_ENEMY_SPEED;
-        this.reward = GameConfig.NORMAL_ENEMY_REWARD;
-        this.image = new Image(GameConfig.NORMAL_ENEMY_IMAGE_URL);
+        this.health = GameConfig.SMALLER_ENEMY_HEALTH;
+        this.armor = GameConfig.SMALLER_ENEMY_ARMOR;
+        this.speed = GameConfig.SMALLER_ENEMY_SPEED;
+        this.reward = GameConfig.SMALLER_ENEMY_REWARD;
+        this.image = new Image("file:src/Assets/Enemy/Abstract Enemy/towerDefense_tile247.png");
         this.imageV = new ImageView(image);
         TowerDefense.root.getChildren().add(imageV);
     }
