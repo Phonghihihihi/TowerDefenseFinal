21a37866f06fd63cb71e136a4b591961aced0098
khang <43407475+khangneko@users.noreply.github.com>
2019-12-09 23:01:46 +0700

    a

--- src/module-info.java
+++ src/module-info.java
@@ -2,10 +2,7 @@ module TowerDefenseFinal {
     requires javafx.fxml;
     requires javafx.controls;
     requires java.desktop;
-
     requires javafx.media;
-
-    requires java.desktop;
 
     opens towerdefense.ui;
 }
